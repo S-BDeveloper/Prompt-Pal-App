ca88a15f5d2ae15ed429b88d9e52488e8d968ab8
Sabina Begum <begumsabina81193@gmail.com>
2026-01-31 20:19:19 +0000

    test(S4): add Jest mock for gesture-handler and fix rateLimiter Jest globals

--- PromptPal/jest.config.js
+++ PromptPal/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^react-native-gesture-handler$': '<rootDir>/__mocks__/react-native-gesture-handler.js',
   },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|nativewind))',
